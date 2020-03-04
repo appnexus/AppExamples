737bd92fa75718365bac0a4605c66fffe4c12a01
Wei Zhang <wzhang@appnexus.com>
2020-03-04 15:42:59 -0500

    app update

--- Android/Java/FacebookDemand/app/src/main/java/com/xandr/facebookdemand/MainActivity.java
+++ Android/Java/FacebookDemand/app/src/main/java/com/xandr/facebookdemand/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,8 +22,14 @@ import com.appnexus.opensdk.NativeAdResponse;
 import com.appnexus.opensdk.NativeAdSDK;
 import com.appnexus.opensdk.ResultCode;
 import com.appnexus.opensdk.SDKSettings;
+import com.appnexus.opensdk.csr.FBNativeBannerAdResponse;
+import com.facebook.ads.AdOptionsView;
 import com.facebook.ads.AudienceNetworkAds;
+import com.facebook.ads.NativeAdBase;
 import com.facebook.ads.NativeAdLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,12 +56,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void removePreviousAd() {
         if (this.response != null) {
-//            if (this.response instanceof FBNativeBannerAdResponse) {
-//                FBNativeBannerAdResponse fbresponse = (FBNativeBannerAdResponse) response;
-//                fbresponse.unregisterView();
-//                this.response = null;
-//            }
-            NativeAdSDK.unRegisterTracking(findViewById(R.id.native_ad_view));
+            if (this.response instanceof FBNativeBannerAdResponse) {
+                FBNativeBannerAdResponse fbresponse = (FBNativeBannerAdResponse) response;
+                fbresponse.unregisterView();
+                this.response = null;
+            } else {
+                NativeAdSDK.unRegisterTracking(findViewById(R.id.native_ad_view));
+            }
         }
         NativeAdLayout nativeAdLayout = findViewById(R.id.native_banner);
         nativeAdLayout.removeAllViews();
@@ -74,14 +82,13 @@ public class MainActivity extends AppCompatActivity {
             public void onAdLoaded(NativeAdResponse response) {
                 Log.d(LOG_TAG, "loaded");
                 MainActivity.this.response = response;
-//                if (response instanceof FBNativeBannerAdResponse) {
-//                    Log.d("NativeBanner", "FB ad loaded");
-//                    FBNativeBannerAdResponse fbResponse = (FBNativeBannerAdResponse) response;
-//                    inflateAndRegisterFB(fbResponse);
-//                } else {
-//                    inflateAndRegisterNonFB(response);
-//                }
-                inflateAndRegisterNonFB(response);
+                if (response instanceof FBNativeBannerAdResponse) {
+                    Log.d("NativeBanner", "FB ad loaded");
+                    FBNativeBannerAdResponse fbResponse = (FBNativeBannerAdResponse) response;
+                    inflateAndRegisterFB(fbResponse);
+                } else {
+                    inflateAndRegisterNonFB(response);
+                }
             }
 
             @Override
@@ -135,56 +142,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//    private void inflateAndRegisterFB(FBNativeBannerAdResponse response) {
-//
-//        // Add the Ad view into the ad container.
-//
-//        NativeAdLayout nativeAdLayout = findViewById(R.id.native_banner);
-//        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
-//        // Inflate the Ad view.  The layout referenced is the one you created in the last step.
-//        LinearLayout adView = (LinearLayout) inflater.inflate(R.layout.native_banner, nativeAdLayout, false);
-//        nativeAdLayout.addView(adView);
-//
-//        // Add the AdChoices icon
-//        RelativeLayout adChoicesContainer = adView.findViewById(R.id.ad_choices_container);
-//        AdOptionsView adOptionsView = new AdOptionsView(MainActivity.this, (NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT), nativeAdLayout);
-//        adChoicesContainer.removeAllViews();
-//        adChoicesContainer.addView(adOptionsView, 0);
-//
-//        // Create native UI using the ad metadata.
-//        TextView nativeAdTitle = adView.findViewById(R.id.native_ad_title);
-//        TextView nativeAdSocialContext = adView.findViewById(R.id.native_ad_social_context);
-//        TextView sponsoredLabel = adView.findViewById(R.id.native_ad_sponsored_label);
-//        ImageView nativeAdIconView = adView.findViewById(R.id.native_icon_view);
-//        Button nativeAdCallToAction = adView.findViewById(R.id.native_ad_call_to_action);
-//
-//        // Set the Text.
-//        nativeAdCallToAction.setText(response.getCallToAction());
-//        nativeAdCallToAction.setVisibility(!TextUtils.isEmpty(response.getCallToAction()) ? View.VISIBLE : View.INVISIBLE);
-//        nativeAdTitle.setText(((NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT)).getAdvertiserName());
-//        nativeAdSocialContext.setText(((NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT)).getAdSocialContext());
-//        sponsoredLabel.setText(((NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT)).getSponsoredTranslation());
-//
-//        // Register the Title and CTA button to listen for clicks.
-//        List<View> clickableViews = new ArrayList<>();
-//        clickableViews.add(nativeAdTitle);
-//        clickableViews.add(nativeAdCallToAction);
-//        response.registerView(adView, nativeAdIconView, clickableViews, new NativeAdEventListener() {
-//            @Override
-//            public void onAdWasClicked() {
-//                Log.d("NativeBannerTest", "FB ad was clicked");
-//            }
-//
-//            @Override
-//            public void onAdWillLeaveApplication() {
-//
-//            }
-//
-//            @Override
-//            public void onAdWasClicked(String clickUrl, String fallbackURL) {
-//
-//            }
-//        });
-//
-//    }
+    private void inflateAndRegisterFB(FBNativeBannerAdResponse response) {
+
+        // Add the Ad view into the ad container.
+
+        NativeAdLayout nativeAdLayout = findViewById(R.id.native_banner);
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        // Inflate the Ad view.  The layout referenced is the one you created in the last step.
+        LinearLayout adView = (LinearLayout) inflater.inflate(R.layout.native_banner, nativeAdLayout, false);
+        nativeAdLayout.addView(adView);
+
+        // Add the AdChoices icon
+        RelativeLayout adChoicesContainer = adView.findViewById(R.id.ad_choices_container);
+        AdOptionsView adOptionsView = new AdOptionsView(MainActivity.this, (NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT), nativeAdLayout);
+        adChoicesContainer.removeAllViews();
+        adChoicesContainer.addView(adOptionsView, 0);
+
+        // Create native UI using the ad metadata.
+        TextView nativeAdTitle = adView.findViewById(R.id.native_ad_title);
+        TextView nativeAdSocialContext = adView.findViewById(R.id.native_ad_social_context);
+        TextView sponsoredLabel = adView.findViewById(R.id.native_ad_sponsored_label);
+        ImageView nativeAdIconView = adView.findViewById(R.id.native_icon_view);
+        Button nativeAdCallToAction = adView.findViewById(R.id.native_ad_call_to_action);
+
+        // Set the Text.
+        nativeAdCallToAction.setText(response.getCallToAction());
+        nativeAdCallToAction.setVisibility(!TextUtils.isEmpty(response.getCallToAction()) ? View.VISIBLE : View.INVISIBLE);
+        nativeAdTitle.setText(((NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT)).getAdvertiserName());
+        nativeAdSocialContext.setText(((NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT)).getAdSocialContext());
+        sponsoredLabel.setText(((NativeAdBase) response.getNativeElements().get(NativeAdResponse.NATIVE_ELEMENT_OBJECT)).getSponsoredTranslation());
+
+        // Register the Title and CTA button to listen for clicks.
+        List<View> clickableViews = new ArrayList<>();
+        clickableViews.add(nativeAdTitle);
+        clickableViews.add(nativeAdCallToAction);
+        response.registerView(adView, nativeAdIconView, clickableViews, new NativeAdEventListener() {
+            @Override
+            public void onAdWasClicked() {
+                Log.d("NativeBannerTest", "FB ad was clicked");
+            }
+
+            @Override
+            public void onAdWillLeaveApplication() {
+
+            }
+
+            @Override
+            public void onAdWasClicked(String clickUrl, String fallbackURL) {
+
+            }
+        });
+
+    }
 }
